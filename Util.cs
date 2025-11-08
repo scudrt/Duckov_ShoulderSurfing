e358204f2b6cfcea523d215b01fb62bb93745cfe
VanxeHus <13751378996@163.com>
2025-11-08 12:10:17 +0800

    util增加命名空间防止冲突

--- Util.cs
+++ Util.cs
@@ -1,24 +1,29 @@
 ﻿using System.Reflection;
 using UnityEngine;
 
-public static class Util {
-    public static Sprite LoadSprite(string textureName)
+namespace ShoulderSurfing
+{
+    public static class Util
     {
-        string directoryName = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-		string path = Path.Combine(directoryName, "textures");
-		string text = Path.Combine(path, textureName);
-		bool flag2 = File.Exists(text);
-        if (flag2)
+        public static Sprite LoadSprite(string textureName)
         {
-            byte[] data = File.ReadAllBytes(text);
-            Texture2D texture2D = new Texture2D(2, 2);
-            bool flag3 = texture2D.LoadImage(data);
-            if (flag3)
+            string directoryName = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            string path = Path.Combine(directoryName, "textures");
+            string text = Path.Combine(path, textureName);
+            bool flag2 = File.Exists(text);
+            if (flag2)
             {
-                return Sprite.Create(texture2D, new Rect(0f, 0f, (float)texture2D.width, (float)texture2D.height), new Vector2(0.5f, 0.5f), 100f);
+                byte[] data = File.ReadAllBytes(text);
+                Texture2D texture2D = new Texture2D(2, 2);
+                bool flag3 = texture2D.LoadImage(data);
+                if (flag3)
+                {
+                    return Sprite.Create(texture2D, new Rect(0f, 0f, (float)texture2D.width, (float)texture2D.height), new Vector2(0.5f, 0.5f), 100f);
+                }
             }
+            return null;
         }
-        return null;
     }
 }
+
 
